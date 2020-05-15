8d64bdee96f927a2df016b4e63b80e6cd95a818e
Скробот нтон иколаевич <ANSkrobot@sberbank.ru>
2020-05-15 15:10:33 +0300

    trying to fix not resolved secret_key problem, add new setting files

---
+++ empty_site/settings/prod.py
@@ -0,0 +1,22 @@
+from .base import *
+import os
+import django_heroku
+
+SECRET_KEY = os.environ['SECRET_KEY']
+
+django_heroku.settings(locals())
+
+INSTALLED_APPS.append("gunicorn")
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
