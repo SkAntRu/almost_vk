3f8f3c46681ef143491ff108b8156a6e4730d383
Скробот нтон иколаевич <ANSkrobot@sberbank.ru>
2020-05-15 14:03:35 +0300

    add new changes to settings

--- empty_site/settings.py
+++ empty_site/settings.py
@@ -12,6 +12,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]git
+]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
